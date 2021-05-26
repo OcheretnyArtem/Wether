b96b23b48758c278476c3b81331841646df9953d
Gizcerbes <gizcerbes@outlook.com>
2021-05-26 19:24:47 +0300

    create Mapper

--- app/src/main/java/by/ocheretny/weather/mappers/weather/WeatherResponseMapper.kt
+++ app/src/main/java/by/ocheretny/weather/mappers/weather/WeatherResponseMapper.kt
@@ -3,33 +3,42 @@ package by.ocheretny.weather.mappers.weather
 import by.ocheretny.weather.dto.weather.WeatherResponse
 import by.ocheretny.weather.entities.weather.Weather
 import by.ocheretny.weather.mappers.Mapper
+import kotlin.math.max
 
 class WeatherResponseMapper : Mapper<WeatherResponse, Weather> {
     override fun map(from: WeatherResponse): Weather {
         val daily = ArrayList<Weather.Daily>()
-        val temp = ArrayList<Weather.Daily.Temp>()
-
-        from.daily?.forEach {
-            var max = it?.temp?.max
-            var min = it?.temp?.min
-            var Weather.
+        from.daily?.forEach { dl ->
             daily.add(
                 Weather.Daily(
-                    it?.dt,
-                    it?.pop,
-                    max,
-                    min,
-                    it?.uvi,
-                    it?.weather
+                    dt = dl?.dt,
+                    pop = dl?.pop,
+                    temp = Weather.Daily.Temp(
+                        max = dl?.temp?.max,
+                        min = dl?.temp?.min
+                    ),
+                    uvi = dl?.uvi,
+                    weather = ArrayList<Weather.Daily.Weather>().apply {
+                        dl?.weather?.forEach { wea ->
+                            add(
+                                Weather.Daily.Weather(
+                                    description = wea?.description,
+                                    icon = wea?.icon,
+                                    id = wea?.id,
+                                    main = wea?.main
+                                )
+                            )
+                        }
+                    }
                 )
             )
+
         }
         return Weather(
-            daily = from.daily[1].,
+            daily = daily,
             lat = from.lat ?: 0.0,
             lon = from.lon ?: 0.0,
             timezone = from.timezone.orEmpty(),
-
-            )
+        )
     }
 }
